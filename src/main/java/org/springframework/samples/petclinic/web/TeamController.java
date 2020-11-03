813f992b76084b7199dd0336fd16ad1e57c3b9fa
Joaquin Gonzalez <joagonrui1@alum.us.es>
2020-11-03 18:10:49 +0100

    first user story

---
+++ src/main/java/org/springframework/samples/petclinic/web/TeamController.java
@@ -0,0 +1,74 @@
+package org.springframework.samples.petclinic.web;
+
+import java.util.Date;
+import java.util.regex.Pattern;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Manager;
+import org.springframework.samples.petclinic.model.Team;
+import org.springframework.samples.petclinic.service.ManagerService;
+import org.springframework.samples.petclinic.service.TeamService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TeamController {
+
+	private final TeamService teamService;
+	private final ManagerService managerService;
+
+	@Autowired
+	public TeamController(TeamService teamService, ManagerService managerService) {
+		this.teamService = teamService;
+		this.managerService = managerService;
+
+	}
+	
+	@ModelAttribute("manager")
+	public Manager findManager(@PathVariable("managerId") int managerId) {
+		return this.managerService.findManagerById(managerId);
+	}
+        
+
+	@GetMapping(value = "managers/{managerId}/teams/new")
+	public String initCreationForm(Manager manager, ModelMap model) {
+		
+		Integer countedTeams = this.teamService.countTeams(manager.getId());
+		if(countedTeams!=0) {
+			
+			String message = "Ya has creado un equipo";
+			model.put("customMessage", message);
+			return "exception";
+		}
+		
+		Team team = new Team();
+		
+		team.setManager(manager);
+		model.put("team", team);
+		return "teams/create";
+	}
+
+	@PostMapping(value = "managers/{managerId}/teams/new")
+	public String processCreationForm(Manager manager, @Valid Team team, BindingResult result, ModelMap model) {
+		if (result.hasErrors()) {
+			model.put("team", team);
+			return "teams/create";
+		} else {
+
+			team.setManager(manager);
+			Date creation = new Date();
+			team.setCreationDate(creation);
+			this.teamService.saveTeam(team);
+			System.out.println(team);
+			return "redirect:/welcome";
+		}
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/teams/create.jsp
@@ -0,0 +1,32 @@
+<%@ page session="false" trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="petclinic" tagdir="/WEB-INF/tags" %>
+
+<petclinic:layout pageName="teams">
+    <h2>
+        <c:if test="${teams['new']}">New </c:if> Teams
+    </h2>
+    <form:form modelAttribute="team" class="form-horizontal" id="add-team-form">
+        <div class="form-group has-feedback">
+            <petclinic:inputField label="Name" name="name"/>
+            <petclinic:inputField label="NIF" name="nif"/>
+
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <c:choose>
+                    <c:when test="${team['new']}">
+                        <button class="btn btn-default" type="submit">Add Team</button>
+                    </c:when>
+                    <c:otherwise>
+                        <button class="btn btn-default" type="submit">Update Teams</button>
+                    </c:otherwise>
+                </c:choose>
+            </div>
+        </div>
+    </form:form>
+</petclinic:layout>
