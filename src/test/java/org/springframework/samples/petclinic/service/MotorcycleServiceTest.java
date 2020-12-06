78e46c5de69eeb279259a5938c81c9e127ed9ea1
Diego <scoobydoobydoo113@gmail.com>
2020-12-06 12:15:10 +0100

    Creacion caso negativo en moto, y creacion validate y metodo en controller de moto

--- src/test/java/org/springframework/samples/petclinic/service/MotorcycleServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/MotorcycleServiceTest.java
@@ -1,9 +1,11 @@
 package org.springframework.samples.petclinic.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Collection;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,6 +28,7 @@ public class MotorcycleServiceTest {
 	protected PilotService pilotService;
 
 	private Motorcycle motorcycle;
+	private Motorcycle motorcycle2;
 	private Pilot piloto;
 
 	@BeforeEach
@@ -33,8 +36,7 @@ public class MotorcycleServiceTest {
 
 		// Se obtiene el piloto con Id = 3
 		piloto = pilotService.findById(3);
-    motorcycle = this.motorcycleService.findMotorcycleById(1);
-		
+		motorcycle = this.motorcycleService.findMotorcycleById(1);
 
 	}
 
@@ -84,6 +86,47 @@ public class MotorcycleServiceTest {
 		assertThat(motorcycles.size()).isEqualTo(2);
 		assertThat(motorcycle.getBrand()).isEqualTo(brand);
 	}
+  	
+
+	// Casos negativos
+
+	@Test
+	@DisplayName("Edit moto incorrectly")
+	@Transactional
+	void shouldThrowExceptionEditingMotorcycleIncorrectParameter() throws DataAccessException {
+
+		motorcycle2 = new Motorcycle();
+		motorcycle2.setId(4);
+		motorcycle2.setBrand("");
+		motorcycle2.setDisplacement(-1999);
+		motorcycle2.setHorsePower(-350);
+		motorcycle2.setMaxSpeed(-370.5);
+		motorcycle2.setWeight(-140);
+		motorcycle2.setPilot(piloto);
+		motorcycle2.setTankCapacity(-20.5);
+
+
+		this.motorcycleService.saveMoto(motorcycle2);
+		assertThrows(ConstraintViolationException.class, () -> {
+			this.motorcycleService.saveMoto(motorcycle2);
+		});
+	}
+
+//  	@Test //-
+//	@Transactional
+//	public void animalShelterShouldNotCreateRecord() throws NullPointerException {
+//		Owner owner = this.ownerService.findOwnerByUsername("Etereo"); //No existe
+//		Owner animalshelter = this.ownerService.findOwnerById(11);
+//		Record record = new Record();
+//		//Al no existir el owner, surge una NullPointerException
+//		assertThrows(NullPointerException.class, () -> {
+//			record.setAnimalshelter(animalshelter);
+//			record.setOwner(owner);
+//			record.setOwner_id(owner.getId());
+//			this.recordService.saveRecord(record);
+//
+//		});
+//
+//	}
 
 }
-
