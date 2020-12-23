e89502417bedf94ec0e7d15235bc1ad3c04b6f08
Joaquin Gonzalez <joagonrui1@alum.us.es>
2020-12-23 19:09:53 +0100

    Adding mechanic controller tests

--- src/test/java/org/springframework/samples/petclinic/web/TeamControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/TeamControllerTest.java
@@ -10,10 +10,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.springframework.transaction.annotation.Transactional;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -22,7 +22,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.model.Manager;
+import org.springframework.samples.petclinic.model.Mechanic;
 import org.springframework.samples.petclinic.model.Team;
+import org.springframework.samples.petclinic.model.Type;
 import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.service.AuthoritiesService;
 import org.springframework.samples.petclinic.service.ManagerService;
@@ -62,20 +64,43 @@ class TeamControllerTest {
 
 	private static final int TEST_MANAGER_ID = 6;
 	private static final int TEST_TEAM_ID = 3;
-	private Manager manager1;
+	
+	private static final int TEST_MANAGER_ID1 = 1;
+	private static final int TEST_TEAM_ID1 = 1;
+	
 
 	@BeforeEach
 	void setUp() throws Exception {
-		
 
 		User user = new User();
 		user.setUsername("jantontio");
 		user.setPassword("parejo");
 		user.setEnabled(true);
 		
+
+		User user2 = new User();
+		user2.setUsername("breathOf");
+		user2.setPassword("theWild");
+		user2.setEnabled(true);
+		
+		Mechanic mc = new Mechanic();
+		mc.setUser(user2);
+		LocalDate date  = LocalDate.now();
+		mc.setBirthDate(date);
+		mc.setFirstName("Alejandro");
+		mc.setLastName("Dumas");
+		mc.setDni("23124563D");
+		mc.setNationality("Russian");
+		mc.setResidence("Moscu");
+		mc.setType(Type.ENGINE);
+		mc.setId(7);
+		
+		Set<Mechanic> setM = new HashSet<>();
+		setM.add(mc);
+		
 		Manager m = new Manager();
 		m.setUser(user);
-		LocalDate date  = LocalDate.now();
+		//LocalDate date  = LocalDate.now();
 		m.setBirthDate(date);
 		m.setFirstName("Jose Antontio");
 		m.setLastName("Parejo");
@@ -91,6 +116,7 @@ class TeamControllerTest {
 		team1.setName("Ducati");
 		team1.setManager(m);
 		team1.setNif("12345678X");
+		team1.setMechanic(setM);
 		
 		given(this.managerService.findOwnerByUserName())
 		.willReturn(m);
@@ -100,9 +126,20 @@ class TeamControllerTest {
 		
 		given(this.teamService.findManager(TEST_MANAGER_ID)).willReturn(team1);
 		
+		given(this.teamService.getMechanicsById(TEST_TEAM_ID)).willReturn(setM);
+		
+		given(this.teamService.findTeamById(TEST_TEAM_ID)).willReturn(team1);
 		
 		
-		
+	}
+	
+	@WithMockUser(value = "jantontio", authorities = "manager")
+	@Test
+	void testShowTeam() throws Exception {
+		mockMvc.perform(get("/managers/{managerId}/teams/{teamId}/details", TEST_MANAGER_ID,TEST_TEAM_ID))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("team"))
+		.andExpect(view().name("teams/teamDetails"));
 	}
 	
 	@WithMockUser(value = "jantontio", authorities = "manager")
@@ -164,6 +201,45 @@ class TeamControllerTest {
 			.param("manager", "6"))
 			.andExpect(status().is3xxRedirection())
 			.andExpect(view().name("redirect:/managers/6/teams/3/details"));
+	}
+	
+	
+	@WithMockUser(value = "jantontio", authorities = "manager")
+	@Test
+	void testCreateMechanicFormSuccess() throws Exception {
+		mockMvc.perform(post("/managers/{managerId}/teams/{teamId}/mechanics/new", TEST_MANAGER_ID,TEST_TEAM_ID)
+				.with(csrf())
+				.param("firstName", "Antonio")
+				.param("lastName", "Banderas")
+				.param("birthDate", "1960/01/01")
+				.param("residence", "CUEVA")
+				.param("nationality", "SPAIN")
+				.param("dni", "11223344A")
+				.param("type", "ENGINE")
+				.param("user.username", "mechanic6")
+				.param("user.password", "thisisperfectpasswrod123"))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(view().name("redirect:/welcome"));
+	}
+	
+	@WithMockUser(value = "jantontio", authorities = "manager")
+	@Test
+	void testCreateMechanicFormHasErrors() throws Exception {
+		mockMvc.perform(post("/managers/{managerId}/teams/{teamId}/mechanics/new", TEST_MANAGER_ID,TEST_TEAM_ID)
+				.with(csrf())
+				.param("firstName", "Antonio")
+				.param("lastName", "Banderas")
+				.param("birthDate", "1960/01/01")
+				.param("residence", "CUEVA")
+				.param("nationality", "SPAIN")
+				.param("dni", "1hsdfkj3344A")
+				.param("type", "ENGINE")
+				.param("user.username", "mechanic6")
+				.param("user.password", "thisisperfectpasswrod123"))
+				.andExpect(model().attributeHasErrors("mechanic"))
+				.andExpect(model().attributeHasFieldErrors("mechanic", "dni"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("mechanics/createOrUpdateMechanicForm"));
 	}
 
 
