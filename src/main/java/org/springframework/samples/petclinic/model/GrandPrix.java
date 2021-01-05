4c858e39eb9168ead40c607ca69ce068423951a2
César <cesgaldor@alum.us.es>
2021-01-05 12:10:30 +0100

    Modelo de gran premio

---
+++ src/main/java/org/springframework/samples/petclinic/model/GrandPrix.java
@@ -0,0 +1,60 @@
+package org.springframework.samples.petclinic.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "grandPrix")
+public class GrandPrix extends BaseEntity {
+
+	@Column(name = "location")
+	@NotEmpty
+	@Size(min = 3, max =50)
+	private String location;
+
+	@Column(name = "circuit")
+	@NotEmpty
+	@Size(min = 3, max =50)
+	private String circuit;
+
+	@Column(name = "laps")
+	@NotNull
+	@Range(min=0, max=25)
+	private Integer laps;
+	
+	@Column(name = "distance")
+	@NotNull
+	@Range(min=0, max=200)
+	private Double distance;
+	
+	@Column(name = "dayOfRace")
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@NotNull
+	private LocalDate dayOfRace;
+
+	@OneToMany
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	Pilot pilot;
+	
+	@OneToMany
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	Team team;
+
+}
